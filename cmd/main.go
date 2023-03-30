ab08942bf1542d02a578c2ffa8389a206417891c
maks <m.morozov@autodoc.eu>
2023-03-30 15:42:37 +0300

    replace with correct auditor

---
+++ cmd/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"context"
+	"racecondition/auditor"
+	"time"
+)
+
+type myRepository struct {
+}
+
+type Value string
+
+func (v Value) Value() string {
+	return string(v)
+}
+
+func (r *myRepository) CreateMany(ctx context.Context, enities []auditor.Valuable) (int, error) {
+	return 0, nil
+}
+
+func main() {
+	a := auditor.New(new(myRepository))
+	val1 := Value(`Some value 1`)
+	val2 := Value(`Some value 2`)
+
+	a.Update(val1)
+	a.Update(val2)
+
+	time.Sleep(time.Second * 5)
+
+	a.Update(val1)
+	a.Update(val2)
+
+	time.Sleep(time.Second * 5)
+}
--- mvc/go.mod
+++
@@ -1,3 +0,0 @@
-module mvc
-
-go 1.19
